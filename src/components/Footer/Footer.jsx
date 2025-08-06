5a0a5985f0c542bf7eb1595bb6ed7130ce7ca87b
CodeLaMat <eyvaz.alishov@gmail.com>
2025-08-06 14:25:19 +0300

    Update copyright year in Footer component from 2024 to 2025

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -44,7 +44,7 @@ const Footer = () => {
             </section>
           </MDBContainer>
           <div className={classes.copyright}>
-            © 2024 Copyright: Eyvaz Alishov
+            © 2025 Copyright: Eyvaz Alishov
           </div>
         </MDBFooter>
       </div>
